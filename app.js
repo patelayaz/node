f57c1f3e444a9936a334979b2e6fb4da4ef235b7
patelayaz <patelayaz1428@gmail.com>
2022-01-10 21:31:33 -1200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const app = express(); 
 
-// connecting to the localhost mongodb server
-mongoose.connect(`mongodb://localhost:27017/propertyApp`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
 
 // custom middleware
 const catchAsync = require(`./utils/catchAsync`);
 const ExpressError = require(`./utils/ExpressError`);
 
 
-app.get('/',(req,res)=> {
-    res.send("welcome to home page1");
-
-}); 
+// route management
+app.get(`/`, (req, res) => {
+  res.render(`home`);
+});
 const  port = process.env.port || 3000;
 app.listen(port,()=> {
     console.log("guc");
