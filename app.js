74805c6749798f7a88425a447731c5c99ce3e3ef
patelayaz <patelayaz1428@gmail.com>
2022-01-10 21:25:42 -1200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require(`mongoose`);
 const app = express(); 
+const methodOverride = require("method-override");
 
 // custom middleware
 const catchAsync = require(`./utils/catchAsync`);
