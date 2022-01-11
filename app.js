1ef299fb5827cb51d37bcd1d61831dbc7a9b102e
patelayaz <patelayaz1428@gmail.com>
2022-01-10 21:01:46 -1200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const app = express();  
+const app = express(); 
+const path = require(`path`);
+const mongoose = require(`mongoose`);
+const methodOverride = require("method-override");
+const morgan = require(`morgan`);
+const ejsMate = require(`ejs-mate`);
+const Joi = require(`joi`);
 
 app.get('/',(req,res)=> {
     res.send("welcome to home page1");
